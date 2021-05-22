74a6d7b51d13d073f0312e961a6cc93d0086331d
teitaraku <teitaraku669@gmail.com>
2021-05-22 20:27:45 +0900

    rails db:migrate

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_22_105017) do
+ActiveRecord::Schema.define(version: 2021_05_22_112318) do
 
   create_table "products", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
