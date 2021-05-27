3dd8166813615d0b9503a60c987ae4ba822d95e0
teitaraku <teitaraku669@gmail.com>
2021-05-28 02:04:30 +0900

    rename frontend to site

---
+++ site/src/pages/Home.js
@@ -0,0 +1,34 @@
+import React from "react";
+import useApi from 'react-use-api'
+
+const Home = () => {
+  // const [data, { loading, error }, request] = useApi({
+  //   url: 'http://localhost:3000/products.json'
+  // })
+  // {loading && <div>Loading...</div>}
+  // {error && <div>error</div>}
+
+  data = [
+    {id: 1, name: "aaa", campany: "aabbdcc", price: 500},
+
+  ]
+  return (
+    <>
+      <h1>ぐみらぼ</h1>
+      <p>現在登録ぐみ数30件です</p>
+
+      {data && (
+        data.map((product) => (
+          <div key={product.id}>
+            <li>{product.name}</li>
+            <li>{product.company}</li>
+            <li>{product.price}円</li>
+          </div>
+        ))
+      )}
+      <button onClick={request}>Reload</button>
+    </>
+  );
+}
+
+export default Home
