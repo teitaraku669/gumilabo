5cb1a61a618d02c53dd1be4b10c5fa070a95b321
teitaraku <teitaraku669@gmail.com>
2021-05-28 03:16:16 +0900

    add css

--- site/src/pages/Home.js
+++ site/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import HomeCss from './Home.module.css';
 // import useApi from 'react-use-api'
 
 const Home = () => {
@@ -9,23 +10,34 @@ const Home = () => {
   // {error && <div>error</div>}
   // <button onClick={request}>Reload</button>
   const data = [
-    [1,"aaa", "aabbdcc", 500],
-
+    ["UHA味覚糖", "コロロ", "コーラ", "http://uha-cororo.jp/"],
+    ["UHA味覚糖", "コロロ", "ソーダ", "http://uha-cororo.jp/"],
+    ["UHA味覚糖", "コロロ", "グレープ", "http://uha-cororo.jp/"],
+    ["UHA味覚糖", "コロロ", "マスカット", "http://uha-cororo.jp/"],
+    ["UHA味覚糖", "さけるグミ", "巨峰", "https://www.uha-mikakuto.co.jp/sakeru/"],
+    ["UHA味覚糖", "さけるグミ", "コーラ", "https://www.uha-mikakuto.co.jp/sakeru/"],
+    ["UHA味覚糖", "なが〜いさけるグミ", "巨峰", "https://www.uha-mikakuto.co.jp/sakeru/"], 
+    ["UHA味覚糖", "UHAグミサプリ", "めぐみアイ", "http://gummy-supple.com/lineup/megumiai.php"],
+    ["UHA味覚糖", "UHAグミサプリ", "鉄＆葉酸", "http://gummy-supple.com/lineup/age_folate.php"],
+    ["UHA味覚糖", "UHAグミサプリ", "亜鉛", "http://gummy-supple.com/lineup/zinc.php"],
   ]
   return (
     <>
-      <h1>ぐみらぼ</h1>
-      <p>現在登録ぐみ数30件です</p>
-
+      <h1 className={HomeCss.title}>ぐみらぼ</h1>
+      <div className={HomeCss.count}>現在登録ぐみ数 {data.length} 件です</div>
+      <div className={HomeCss.product_list}>
       {data && (
-        data.map((product) => (
-          <div key={product[0]}>
+        data.map((product, index) => (
+          <a href={product[3]} target="_blank" rel="noopener noreferrer">
+          <div key={index} className={HomeCss.product}>
+            <li>{product[0]}</li>
             <li>{product[1]}</li>
             <li>{product[2]}</li>
-            <li>{product[3]}円</li>
           </div>
+          </a>
         ))
       )}
+      </div>
     </>
   );
 }
