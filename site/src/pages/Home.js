7a539ec0fb8cfb5a58691f39157e8b30c55b5727
teitaraku <teitaraku669@gmail.com>
2021-05-30 01:02:48 +0900

    fix layout for pc and sp

--- site/src/pages/Home.js
+++ site/src/pages/Home.js
@@ -23,16 +23,16 @@ const Home = () => {
   ]
   return (
     <>
-      <h1 className={HomeCss.title}>ぐみらぼ</h1>
+      <div className={HomeCss.title}>ぐみらぼ</div>
       <div className={HomeCss.count}>現在登録ぐみ数 {data.length} 件です</div>
       <div className={HomeCss.product_list}>
       {data && (
         data.map((product, index) => (
           <a href={product[3]} target="_blank" rel="noopener noreferrer">
           <div key={index} className={HomeCss.product}>
-            <li>{product[0]}</li>
-            <li>{product[1]}</li>
-            <li>{product[2]}</li>
+            {product[0]}<br/>
+            {product[1]}<br/>
+            {product[2]}<br/>
           </div>
           </a>
         ))
